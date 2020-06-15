c24552b7f851486364586cc5352f49052328a911
pagos21 <paga@localhost.yeah>
2020-06-15 16:01:12 +0200

    v 1.0

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+var kmE = 0.21
+
+function myFunction() {
+  var name = document.getElementById("Name").value;
+  var eta = document.getElementById("eta").value;
+  var km = document.getElementById("km").value;
+
+  if (eta === "") {
+    alert("Il campo età non può essere vuoto!")
+  }
+
+  if (km === "") {
+    alert("Il campo km non può essere vuoto!")
+  }
+
+  if (/\D/.test(eta)) {
+    alert("Non sono ammessi caratteri nel campo età :)");
+  }
+
+  if (/\D/.test(km)) {
+    alert("Non sono ammessi caratteri nel campo km :)");
+  }
+
+  if(eta < 18) {
+    var prezzoL = kmE * km;
+    var prezzoPer =  prezzoL * (20 / 100);
+    var prezzoIn = prezzoL - prezzoPer;
+    var prezzoF = prezzoIn.toFixed(2);
+  }
+
+  else if (eta >= 65) {
+    var prezzoL = kmE * km;
+    var prezzoPer =  prezzoL * (40 / 100);
+    var prezzoIn = prezzoL - prezzoPer;
+    var prezzoF = prezzoIn.toFixed(2);
+  }
+
+  else{
+    var prezzoL = kmE * km;
+    var prezzoF = prezzoL.toFixed(2);
+    }
+
+
+  document.getElementById("risultato").innerHTML = "Ciao " + name + ", spenderai " + prezzoF + "€";
+}
